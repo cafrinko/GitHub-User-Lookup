97b225fdafb2be3a40b65d00d872e50484505719
Catherine Karra <catherine.karra@gmail.com>
2016-06-01 22:32:19 -0700

    adds part of the getRepos prototype method

--- js/github_request.js
+++ js/github_request.js
@@ -1,12 +1,17 @@
 // save apiKey in .env file which is addded to .gitignore or list of files that are ignored when you deploy your application to github or heroku
 // you should not leave your api key in your source code to prevent theft
 var apiKey = require('../.env').apiKey;
+var username =
 
 exports.Search = function(){
 };
 
-exports.Search.prototype.getRepos = function() {
-  console.log("Hi! I'm a weather object.");
+exports.Search.prototype.getRepos = function(){
+$.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+console.log(response);
+}).fail(function(error){
+console.log(error.responseJSON.message);
+});
 };
 
 $(document).ready(function() {
