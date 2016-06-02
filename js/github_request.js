fbcbb4f112447da057dafed869888758b83f6319
Catherine Karra <catherine.karra@gmail.com>
2016-06-01 23:14:08 -0700

    evolves get repos prototype

--- js/github_request.js
+++ js/github_request.js
@@ -7,8 +7,9 @@ exports.Search = function(){
 };
 
 exports.Search.prototype.getRepos = function(){
-$.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-console.log(response);
+$.get('https://api.github.com/users/' + username + '/repos' + '?access_token=' + apiKey).then(function(response){
+  for(i = 0; i < response.length; i++) {
+    $('.showRepos').append(response.repository.name + response.repository.description)
 }).fail(function(error){
 console.log(error.responseJSON.message);
 });
