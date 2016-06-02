8da95f73a2b27b2cf7f5d5502ed7b6002e68b962
Catherine Karra <catherine.karra@gmail.com>
2016-06-01 23:40:39 -0700

    adds remainder of get prototype function conditions

--- js/github_request.js
+++ js/github_request.js
@@ -6,15 +6,17 @@ var username =
 exports.Search = function(){
 };
 
-exports.Search.prototype.getRepos = function(){
-$.get('https://api.github.com/users/' + username + '/repos' + '?access_token=' + apiKey).then(function(response){
-  for(i = 0; i < response.length; i++) {
-    $('.showRepos').append(response.repository.name + response.repository.description)
-}).fail(function(error){
-console.log(error.responseJSON.message);
+exports.Search.prototype.getRepos = function(username){
+  $.get('https://api.github.com/users/' + username + '/repos' + '?access_token=' + apiKey).then(function(response){
+    for(i = 0; i < response.length; i++) {
+      if (response[i].description === "") {
+        $('.showRepos').append("<p>" + "Repository Name: " + response[i].name + "</p>")
+      }
+      else {
+        $('.showRepos').append("<p>" + "Repository Name: " + response[i].name + "</p>" + "<br>" + "<p>" + "Description: " + response[i].description)
+      }
+    }
+  }).fail(function(error){
+    $('.showError').text(error.responseJSON.message);
+  });
 });
-};
-
-$(document).ready(function() {
-  var currentSearchObject = new Search();
-  currentSearchObject.getRepos();
--- js/search-interface.js
+++ js/search-interface.js
@@ -1,2 +1,11 @@
 // require(...) imports the exports package which contains the ajaxRequest function module
 var githubRequest = require('./../js/github_request.js').githubRequest;
+
+$(document).ready(function() {
+  var currentWeatherObject = new Weather();
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    currentWeatherObject.getWeather(city);
+  });
+});
